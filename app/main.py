39dfccf1ad79ea6c9b5424d51be6d5369b06212d
Pradeep <pradeepdhawan@gmail.com>
2023-10-24 14:44:18 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ app = FastAPI()
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
-
-
+    
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
