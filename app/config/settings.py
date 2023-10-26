ace248a09f2a3ac400840b3155539d31c74636f0
Pradeep Dhawan <pradeepdhawan@gmail.com>
2023-10-26 02:01:02 +0100

    changed path to suit linux

--- app/config/settings.py
+++ app/config/settings.py
@@ -12,5 +12,5 @@ class Settings(BaseSettings):
     postgres_db: str
 
     model_config = SettingsConfigDict(
-        env_file=f"{pathlib.Path(__file__).resolve().parent}/.env", extra="ignore"
+        env_file=f"{pathlib.Path(__file__).resolve().parent}\.env", extra="ignore"
     )
